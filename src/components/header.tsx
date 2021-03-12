f1117f2581e5f8f05345acae69e47c760628adae
Igor Garcia <igorgarciaco@gmail.com>
2021-03-12 16:13:08 -0300

    first config

---
+++ src/components/header.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
+interface IProps {
+  siteTitle?: "string"
+}
+
+const Header = ({ siteTitle, ...props }: IProps) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
